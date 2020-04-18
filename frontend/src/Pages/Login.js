92c0edbd8323b9bdd257b101e1f15f71b5aa7604
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-04-18 15:34:24 -0300

    PAgina de login finalizada

--- frontend/src/Pages/Login.js
+++ frontend/src/Pages/Login.js
@@ -1,15 +1,36 @@
-import React from "react";
+import React, { useState, useMemo } from "react";
+import { useHistory } from "react-router-dom";
 
 import twitterLogo from "../twitter.svg";
 import "./Login.css";
 
-export default function Pages() {
+export default function Login() {
+  const history = useHistory();
+
+  const [username, setUsername] = useState("");
+
+  const disabledSubmit = useMemo(() => !username.length, [username]);
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    localStorage.setItem("@GoTwitter:username", username);
+
+    history.push("/timeline");
+  }
+
   return (
     <div className="login-wrapper">
       <img src={twitterLogo} alt="GoTwitter" />
-      <form>
-        <input placeholder="Nome do usuário" />
-        <button type="submit">Entrar</button>
+      <form onSubmit={handleSubmit}>
+        <input
+          placeholder="Nome do usuário"
+          value={username}
+          onChange={(e) => setUsername(e.target.value)}
+        />
+        <button type="submit" disabled={disabledSubmit}>
+          Entrar
+        </button>
       </form>
     </div>
   );
