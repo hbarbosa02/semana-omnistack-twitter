202b9e3589998d505ade7aea6caae2ad1efcd812
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-04-18 10:15:14 -0300

    mongodb add e model Tweet criada

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,6 +1,15 @@
 const express = require("express");
+const mongoose = require("mongoose");
 
 const app = express();
+
+mongoose.connect(
+  "mongodb://admin:admin@cluster0-shard-00-00-qm1bp.mongodb.net:27017,cluster0-shard-00-01-qm1bp.mongodb.net:27017,cluster0-shard-00-02-qm1bp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+  }
+);
 
 app.get("/", (req, res) => {
   return res.send("Hello World");
