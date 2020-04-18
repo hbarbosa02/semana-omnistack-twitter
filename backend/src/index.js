ef56cdfbef890847bf20881371d36e18228fbd9f
hiran.g.b.silva <hiran.g.b.silva@gmail.com>
2020-04-18 10:55:52 -0300

    socket io add

--- backend/src/index.js
+++ backend/src/index.js
@@ -1,9 +1,13 @@
 const express = require("express");
 const mongoose = require("mongoose");
+const cors = require("cors");
 
 const Routes = require("./routes");
 
 const app = express();
+
+const server = require("http").Server(app);
+const io = require("socket.io")(server);
 
 mongoose.connect(
   "mongodb://admin:admin@cluster0-shard-00-00-qm1bp.mongodb.net:27017,cluster0-shard-00-01-qm1bp.mongodb.net:27017,cluster0-shard-00-02-qm1bp.mongodb.net:27017/test?ssl=true&replicaSet=Cluster0-shard-0&authSource=admin&retryWrites=true&w=majority",
@@ -13,9 +17,16 @@ mongoose.connect(
   }
 );
 
+app.use((req, res, next) => {
+  req.io = io;
+
+  return next();
+});
+
+app.use(cors());
 app.use(express.json());
 app.use(Routes);
 
-app.listen(4000, () => {
+server.listen(4000, () => {
   console.log("Server started on port 4000");
 });
